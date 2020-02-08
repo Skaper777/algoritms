37792ae9c310e2c74583c311826e7bda288211f8
Skaper777 <skaper777@gmail.com>
2020-02-08 20:19:52 +0300

    binary full

--- scripts/binary-search.js
+++ scripts/binary-search.js
@@ -1,3 +1,36 @@
-const array = [2, 6, 4, 9, 1, 0, 15, 24, 13];
+const array = [];
+let lengthArray = 100000;
 
-console.log(array);
+for (let i = 0; i < lengthArray; i++) {
+  array.push(i + 1);
+}
+
+const binarySearch = (num) => {
+  console.log(array, array.length);
+  
+  let low = 0;
+  let high = array.length - 1;
+  let step = 1;
+
+  while (low <= high) {
+    const middle = Math.floor((low + high) / 2);
+    const guess = array[middle];
+
+    console.log('step: ' + step++);
+    console.log(guess);
+
+    if (guess < num) {
+      low = middle + 1;
+    } else if (guess > num) {
+      high = middle - 1;
+    } else {
+      console.log('Индекс числа: ' + middle + ' | steps: ' + (step - 1));
+      return middle;
+    }
+  }
+
+  console.log('Нет значения');
+  return null;
+};
+
+binarySearch(100);
