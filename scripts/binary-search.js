78eea1508f6ac4288b2a71b0e137da247f30ac3a
Skaper777 <skaper777@gmail.com>
2020-02-17 19:09:45 +0300

    selection-sort

--- scripts/binary-search.js
+++ scripts/binary-search.js
@@ -1,31 +1,35 @@
+/**
+ * Алгоритм бинарного поиска
+ */
+
 const array = [];
-let lengthArray = 100000;
+let lengthArray = 100; // Длина массива
 
 for (let i = 0; i < lengthArray; i++) {
-  array.push(i + 1);
+  array.push(i + 1); // Заполнение массива
 }
 
 const binarySearch = (num) => {
   console.log(array, array.length);
   
-  let low = 0;
-  let high = array.length - 1;
-  let step = 1;
+  let low = 0; // Начало поиска
+  let high = array.length - 1; // Конец поиска
+  let step = 1; // Шаг поиска
 
   while (low <= high) {
-    const middle = Math.floor((low + high) / 2);
-    const guess = array[middle];
+    const middle = Math.floor((low + high) / 2); // Середина списка
+    const guess = array[middle]; // Попытка нахождения числа
 
     console.log('step: ' + step++);
     console.log(guess);
 
     if (guess < num) {
-      low = middle + 1;
+      low = middle + 1; // Если выбранное число меньше загаданного, то смещаем начало на середину + 1
     } else if (guess > num) {
-      high = middle - 1;
+      high = middle - 1; // Если выбранное число больше загаданного, то смещаем конец на середину - 1
     } else {
       console.log('Индекс числа: ' + middle + ' | steps: ' + (step - 1));
-      return middle;
+      return middle; // Находим число
     }
   }
 
---
+++ scripts/selection-sort.js
@@ -0,0 +1,26 @@
+/**
+ * Алгоритм сортировки выбором
+ */
+
+const nums = [5, 3, 8, 22, 10, 1, 26, 11, 54, 110, 4, 2]; // Входной массив
+
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    let min = i; //  storing the index of minimum element
+
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j; // updating the index of minimum element
+      }
+    }
+
+    if (i !== min) { // swapping elements
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr
+}
+
+console.log(selectionSort(nums));
